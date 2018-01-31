56f06df16ad3aa5ea1a1c0ce0bef67572f130d57
joshua-heipel <joshua.heipel@gmx.de>
2018-01-31 12:35:58 +0100

    Added ShinyTwitterExplorer 0.1

---
+++ main.R
@@ -0,0 +1,17 @@
+# install packages
+packages <- c("shiny", "rtweet", "DT", "shinyFiles")
+
+ipak <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+}
+
+ipak(packages)
+
+# load code
+source("ui.R")
+source("server.R")
+
+# run app
+shinyApp(ui, server)
---
+++ server.R
@@ -0,0 +1,116 @@
+# options
+verbose = TRUE
+cols    = c("text", "hashtags", "lang", "label", "image")
+lang    = "de"
+
+# load packages
+library(shiny)
+library(shinyFiles)
+library(DT)
+library(rtweet)
+
+# main
+searchTwitter <- function(query, n) {
+  if (!is.null(query) & query != "") {
+    tweets <- search_tweets(query, n = n, include_rts = FALSE, type="recent", 
+                            retryonratelimit = FALSE, verbose = FALSE,
+                            lang = lang)
+    return(tweets)
+  }
+}
+
+flatten_tweets <- function(tweets) {
+  col_ids <- which(sapply(tweets, mode) == "list" & names(tweets) != "hashtags")
+  tweets[col_ids] <- lapply(tweets[col_ids], function(x) 
+    sapply(x, function(y) if (!is.na(y[1])) paste(y, collapse = " ") else NA))
+  tweets$hashtags <- sapply(tweets$hashtags, function(x) if (!is.na(x[1])) 
+    paste("#", x, sep="", collapse=" ") else NA)
+  Encoding(tweets$text) <- "UTF-8"
+  tweets$status_id <- paste("x", tweets$status_id, sep="")
+  fotoIds <- which(tweets$media_type == "photo")
+  tweets["label"] <- NA
+  tweets["predicted"] <- NA
+  tweets["image"] <- NA
+  tweets$image[fotoIds] <- paste('<img src="', tweets$media_url[fotoIds], '">', sep="")
+  return(tweets)
+}
+
+server <- shinyServer(function(input, output) {
+
+  vals <- reactiveValues()
+  
+  observeEvent(
+    input$searchButton,
+    {
+      tweets <- isolate(searchTwitter(input$inputQuery, as.numeric(input$number)))
+      tweets <- flatten_tweets(tweets)
+      if (verbose) {
+        View(tweets)
+      }
+      vals$data <- tweets
+    }, 
+    ignoreInit = TRUE)
+
+  observeEvent(
+    input$downloadButton,
+    {
+      if (file.exists("data/tweets.csv")) {
+        col.names = FALSE
+      } else {
+        col.names = TRUE
+      }
+      write.table(vals$data[input$data_rows_selected,-c(ncol(vals$data))], 
+                  "data/tweets.csv", sep = ",", row.names = FALSE, 
+                  fileEncoding = "UTF-8", append = TRUE, col.names = col.names)
+      image_ids <- intersect(which(vals$data$media_type == "photo"),
+                             input$data_rows_selected)
+      if (length(image_ids)) {
+        image_tweets <- vals$data[image_ids, c("status_id", "media_url")]
+        apply(image_tweets, 1, function(x) try(download.file(x[2],
+                                               paste("data/", x[1],".jpg",sep=""),
+                                               "auto", quiet = TRUE, mode = "wb")))
+      }
+    },
+    ignoreInit = TRUE)
+  
+  observeEvent(
+    input$labelButton, {
+      vals$data$label[input$data_rows_selected] <- paste(unlist(input$selectedLabel), collapse = " ")
+    }, 
+    ignoreInit = TRUE)
+
+  output$data <- DT::renderDataTable({
+    # cols <- c("text", "hashtags", "lang", "label", "image")
+    if (is.null(vals$data)) {
+      dummy <- data.frame(matrix(NA, 1, 5))
+      names(dummy) <- cols
+      return(dummy)
+    }
+    return(vals$data[cols])
+  }, 
+  escape = FALSE,
+  filter = "none",
+  options = list(searching=FALSE, caseInsensitive=TRUE, smart=TRUE, 
+                 columnDefs = list(list(targets = c(5), searchable=FALSE))))
+})
+
+# output$image = renderUI({
+#   src = twitterData()$media_url[1]
+#   tags$img(src=src, width="100px", height="auto")
+# })
+# 
+
+# output$downloadData <- downloadHandler(
+#   filename = function() {
+#     paste(input$inputQuery, "csv", sep=".")
+#   },
+#   content = function(file) {
+#     data <- vals$data[input$data_rows_selected,]
+#     write.csv(
+#       data, file, row.names = FALSE,
+#       append = FALSE, fileEncoding = "UTF-8"
+#     )
+#   }
+# )
+
+# shinyFileChoose(input, "fileButton", roots=c(wd='.'), filetypes=c('', 'txt'))
